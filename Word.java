deb0155d9ba5492d76224631b89ad428637ff3eb
Christopher Perez <deepBlue808.1@gmail.com>
2021-11-17 19:28:22 -0500

    Letter to Matching Key Functionality Added

--- Word.java
+++ Word.java
@@ -1,9 +1,9 @@
-class Word{
+public class Word{
 	private String word;
-	private short key;
+	private int key;
 	private byte wSize;
 
-	public Word(String word, short key){
+	public Word(String word, int key){
 		this.word = word;
 		this.key = key;
 		wSize = (byte)word.length();
@@ -11,33 +11,65 @@ class Word{
 
 	public Word(String word){
 		this.word = word;
+		wSize = (byte)word.length();
 		this.key = makeKey();
+		
+		
 	}
 
 
-	public void setKey(short key){
+	public void setKey(int key){
 		this.key = key;
 	}
 
 	public void setWord(String word ){
 		this.word = word;
+		wSize = (byte)word.length();
+		this.key = makeKey();
 	}
 
+	public int getKey(){
+		return this.key;
+	}
+	public String getWord(){
+		return this.word;
+	}
 
-	public short makeKey(){
+	public int makeKey(){
 		String holder = "";
 
-		for(int i = 0; i < wSize; i++){
+		for(byte i = 0; i < wSize; i++){
 			holder += getLetterHash(word.charAt(i));
+			
 		}
 
-		return Byte.parseShort(holder);
+		return Integer.parseInt(holder);
 	}
 
 
+	/*
+		The dial standard is a bit tricky since first, the lettwers start from the number 2. 
+		Second and most importantly, the letters aren't event distributed in a pattern that can be put into a simple equation. 
+		For instance PQRS belong to 7 while TUV belong to 8 and WXYZ belong to 9. 
 
-	private short getLetterHash(char curr){
-		return ((curr - 64) / 3) + 1;
+		To accomodate, the simplest option and most efficient was to make a series of if statements to filter the input. 
+
+		The test can be found in testOut.txt
+	*/
+	private int getLetterHash(char curr){
+		int holder =  (curr - 64);
+
+		if (holder >= 23) // letter W or any letter after. 
+			holder = 9;
+		else if (holder >= 20) //letter T or any letter after. 
+			holder = 8;
+		else if (holder >= 16) //letter P or any letter after.
+			holder = 7;
+		else 
+			holder = (int)Math.ceil( (holder/3d) + 1);
+		
+		return holder;
+
 	}
 
 }
